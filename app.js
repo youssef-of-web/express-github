72b8daebae88f65d424f1bd161ab1bacdc539ada
mansouri youssef <mansouriyoussef1991@gmail.com>
2022-03-01 23:14:55 +0100

    comment

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 
 var app = express();
 
-app.use(logger('dev'));
+//app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
